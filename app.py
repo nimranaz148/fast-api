c116837ea102e30462f099f2e5a260f9dc703e07
nimranaz148 <159953826+nimranaz148@users.noreply.github.com>
2025-03-21 02:49:43 +0500

    2

--- app.py
+++ app.py
@@ -1,55 +1,44 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 names: list= ['sabih', 'ali']
 
-# @app.get("/")
-# def data():
-#     return names
-
-
-
-# @app.post("/names")
-# def add_name(name:str):
-#     names.append(name)
-#     return names
-
-# @app.delete("/names/{name}")
-# def del_name(name:str):
-#     names.remove(name)
-#     return names
-    
-# @app.put("/names/{index}")
-# def update(index:int,name:str):
-#     names[index]=name
-#     return names
-    
-    
-    
-    
-    
-    
-    
+class NameRequest(BaseModel):
+    data:str
 
 @app.get("/")
 def data():
     return names
 
-@app.post("/myname")
-def add_data(data:str):
-    names.append(data)
+
+
+@app.post("/names")
+def add_name(request: NameRequest):
+    names.append(request.data)
     return names
 
+# ======================================delete=============
 
-@app.delete("/myname/{index}")
-def del_finc(index:int):
+@app.delete("/names/{index}")
+def del_name(index:int):
     names.pop(index)
     return names
-
-@app.put("/myname/{index}")
-def update(index:int):
-    names[index]= "ascdfer"
+ 
+ 
+#  =================================updatw data================
+@app.put("/names/{index}")
+def update(index:int, request:NameRequest):
+    names[index]= request.data
     return names
+    
+    
+    
+    
+    
+    
+    
+
 
     
